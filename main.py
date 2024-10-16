d198443e9ea3228feb86f2699970a52bc89798cd
mmx <github@m2nx.com>
2024-10-16 08:28:34 +0000

    fix rotate

--- main.py
+++ main.py
@@ -5,13 +5,16 @@ import telebot
 import logging
 import configparser
 import shelve
+import io
+import tempfile
+from moviepy.editor import *
 
 # set config
-logging.basicConfig(encoding='utf-8', level=logging.INFO)
+logging.basicConfig(encoding='utf-8', level=logging.DEBUG)
 requests.packages.urllib3.disable_warnings()
 config = configparser.ConfigParser()
 config.read('config.ini')
-
+print('start config')
 # get params from config parser
 interval_time = int(config['DEFAULT'].get('INTERVAL_TIME', 5))
 is_ssl = config['DEFAULT'].getboolean('USE_SSL', True)
@@ -23,27 +26,40 @@ scheme = 'https://' if is_ssl else 'http://'
 host = scheme + host
 username = config['DEFAULT'].get('USERNAME')
 password = config['DEFAULT'].get('PASSWORD')
+print(config['DEFAULT'])
 
 # start unifi session
+print('start session')
 session = requests.Session()
-session.post(host + '/api/auth/login', data={'username': username, 'password': password, 'remember': True}, verify=False)
+res = session.post(host + '/api/auth/login', data={'username': username, 'password': password, 'remember': True}, verify=False)
+print(res)
 
 # start telegram bot
 telegram_bot = telebot.TeleBot(bot_token, parse_mode=None)
 telegram_bot.send_message(telegram_chat_id, 'bot start running....')
 
+print(1)
 while True:
     # 1. get latest videos json
-    list_motion_videos = session.get(f"{host}/proxy/protect/api/events", params={ 'allCameras': True, 'limit': 5, 'orderDirection': 'DESC', 'types': 'motion'})
+    #list_motion_videos = session.get(f"{host}/proxy/protect/api/events", params={ 'allCameras': True, 'limit': 5, 'orderDirection': 'DESC', 'types': 'motion', 'types': 'smartDetectZone', 'types': 'smartDetectLine', 'types': 'smartAudioDetect', 'types': 'ring', 'types': 'doorAccess'})
+    # motion detection  https://10.10.10.5/proxy/protect/api/detection-search?labels=camera%3A614f625b02de2e03e70003ed&limit=100&orderDirection=DESC
+    list_motion_videos = session.get(f"{host}/proxy/protect/api/detection-search", params={ 'limit': 5, 'orderDirection': 'DESC', 'labels': f'camera:{camera_id}'})
     logging.debug('checking latest motion detection info.')
+    print(2)
 
     # get video info success
     if list_motion_videos.status_code != 200:
+        print(3)
+        logging.info(list_motion_videos.status_code)
+        logging.info(list_motion_videos.content)
         logging.info('get unifi events error.')
+        session.post(host + '/api/auth/login', data={'username': username, 'password': password, 'remember': True}, verify=False)
         continue
 
+    print(4)
     logging.debug('get latest video info success.')
-    videos = json.loads(list_motion_videos.content)
+    videos = json.loads(list_motion_videos.content)['events']
+    print(5)
 
     db = shelve.open('data/shelve', writeback=True)
     # dictionary in shelve data
@@ -53,7 +69,6 @@ while True:
 
     # if video id not in data, then create new one with value False.
     for video in videos:
-
         if db['data'].get(video['id']):
             continue
 
@@ -66,12 +81,38 @@ while True:
             continue
         try:
             logging.info('download success. start sending to telegram.')
-            telegram_bot.send_video(telegram_chat_id, file_motion.content)
+            with tempfile.NamedTemporaryFile(suffix=".mp4", delete=True) as temp_video_file:
+                temp_video_file.write(file_motion.content)
+                temp_video_file.flush()  # Ensure all data is written
+
+                # Load the video from the temporary file
+                clip = VideoFileClip(temp_video_file.name).rotate(270)
+
+                # Save the rotated clip to a new temporary file before sending
+                output_filename = 'rotated_video.mp4'
+                clip.write_videofile(output_filename, codec='libx264')
+
+                # Save the rotated clip as a GIF
+
+                # Send the video to Telegram
+                with open(output_filename, 'rb') as file:
+                    telegram_bot.send_video(telegram_chat_id, file)
+                    #telegram_bot.send_document(telegram_chat_id, file)
+
+            logging.info('Video sent to Telegram.')
+            #print(type(file_motion.content))
+            #video_bytes_io = io.BytesIO(file_motion.content)
+            #print(type(video_bytes_io))
+            #clip = VideoFileClip(video_bytes_io).rotate(90)
+            #print(f'@@@@@@@@@@@@{type(clip)}')
+
+            #telegram_bot.send_video(telegram_chat_id, clip)
+            #telegram_bot.send_video(telegram_chat_id, file_motion.content)
             logging.info(f"video {video['id']} sent.")
             db['data'][video['id']] = True
             # write back from mem to db
             db.sync()
-        except Exception:
-            logging.info('send video failed. please check your network.')
+        except Exception as e:
+            logging.info(f'send video failed. please check your network.{e}')
     db.close()
     time.sleep(interval_time)
